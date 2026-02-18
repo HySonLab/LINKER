0ae304db0bf4cad16900dc68c3303497297ff5fd
Phuc Pham <phuc.phamhuythienai@gmail.com>
2026-02-17 22:44:13 -0600

    Increase training epochs from 10 to 80

--- run/run_Predictor.py
+++ run/run_Predictor.py
@@ -124,6 +124,6 @@ if __name__ == "__main__":
     alignloss = LatentAlignmentLoss(tau=0.1, uniform_weight=0.1).cuda()
     optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
 
-    train_model(model, train_loader, test_loader, regressionloss, alignloss, optimizer, device, num_epochs=10, lambda_value = args.lambda_value, save_path="checkpoints/best_predictor.pt")
+    train_model(model, train_loader, test_loader, regressionloss, alignloss, optimizer, device, num_epochs=80, lambda_value = args.lambda_value, save_path="checkpoints/best_predictor.pt")
     test_model(model, test_loader, regressionloss, alignloss, device, model_path="checkpoints/best_predictor.pt")
 
